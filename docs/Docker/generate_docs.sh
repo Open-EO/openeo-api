6154282548116659439a9549dcec5d83d103241c
Marius Appel <marius.appel@uni-muenster.de>
2018-01-24 14:06:14 +0100

    documentation generation for branches

--- docs/Docker/generate_docs.sh
+++ docs/Docker/generate_docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+BRANCH=${TRAVIS_BRANCH:-master}
 
 cd /opt/spectacle
-curl https://raw.githubusercontent.com/Open-EO/openeo-api-poc/master/swagger.json -O
+curl https://raw.githubusercontent.com/Open-EO/openeo-api-poc/$BRANCH/swagger.json -O
 node bin/spectacle.js -t /tmp/apidoc/ -f index.html -l /shared/docs/openeo_logo.png swagger.json
 
 # cd /opt/widdershins
