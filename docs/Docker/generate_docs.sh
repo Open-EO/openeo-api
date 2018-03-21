efe17a42b1a3ed30663e4eb99c4d078f30d4bb12
Matthias Mohr <webmaster@mamo-net.de>
2018-03-21 12:06:13 +0100

    Re-added lost API documentation generation command

--- docs/Docker/generate_docs.sh
+++ docs/Docker/generate_docs.sh
@@ -4,10 +4,14 @@ export LANG=C.UTF-8
 BRANCH=${TRAVIS_BRANCH:-master}
 echo -e "Generating API documentation for branch ${BRANCH}"
 
+# Generate API documentation
+cd /shared
+mkdocs build --clean
+
+# Generarte API reference
 mkdir -p /shared/site/apireference
 chmod -R 777 /shared/site
 cd /shared/site/apireference
 cp -R /opt/swagger-ui/dist/* .
 curl https://raw.githubusercontent.com/Open-EO/openeo-api/$BRANCH/swagger.json -O
 sed -i -e 's!http://petstore.swagger.io/v2/swagger.json!swagger.json!g' index.html
-
