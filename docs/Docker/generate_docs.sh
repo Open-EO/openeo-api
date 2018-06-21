2de3a1e193de8daa3dfd142853f08893b3059d68
Matthias Mohr <m.mohr@uni-muenster.de>
2018-06-21 13:08:40 +0200

    Fixing API reference generation / Travis Build

--- docs/Docker/generate_docs.sh
+++ docs/Docker/generate_docs.sh
@@ -13,6 +13,7 @@ mkdir -p /shared/site/apireference
 chmod -R 777 /shared/site
 cd /shared/site/apireference
 cp -R /opt/swagger-ui/dist/* .
+curl https://raw.githubusercontent.com/Open-EO/openeo-api/$BRANCH/openapi.json -O
 sed -i -e 's!https://petstore.swagger.io/v2/swagger.json!openapi.json!g' index.html
 cp /opt/redoc.html .
 cp /opt/openapi.json .
