9cbfbac1250f14977b40908554cc54006dfd7c27
Matthias Mohr <m.mohr@uni-muenster.de>
2018-06-21 13:50:39 +0200

    Fixing API reference generation for ReDoc

--- docs/Docker/generate_docs.sh
+++ docs/Docker/generate_docs.sh
@@ -15,4 +15,5 @@ cd /shared/site/apireference
 cp -R /opt/swagger-ui/dist/* .
 curl https://raw.githubusercontent.com/Open-EO/openeo-api/$BRANCH/openapi.json -O
 sed -i -e 's!https://petstore.swagger.io/v2/swagger.json!openapi.json!g' index.html
-cp /opt/redoc.html .
+mkdir redoc
+cp /opt/redoc.html redoc/index.html
